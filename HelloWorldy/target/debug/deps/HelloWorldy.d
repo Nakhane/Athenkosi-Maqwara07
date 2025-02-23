08f9b4a1b651e4e2bf55f243f848c6c7221dbe4c
Nakhane <94124925+Nakhane@users.noreply.github.com>
2025-02-23 20:48:10 +0200

    Delete HelloWorldy directory

--- HelloWorldy/target/debug/deps/HelloWorldy.d
+++
@@ -1,5 +0,0 @@
-C:\Users\c2072289\Documents\HelloWorldy\target\debug\deps\HelloWorldy.exe: src\main.rs
-
-C:\Users\c2072289\Documents\HelloWorldy\target\debug\deps\HelloWorldy.d: src\main.rs
-
-src\main.rs:
