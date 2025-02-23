3d12903c27e7278b1681737a52c4f5bb17fe1288
Nakhane <sitamaqwara@gmail.com>
2025-02-23 16:50:24 +0200

    Testing

---
+++ HelloWorldy/target/debug/deps/HelloWorldy.d
@@ -0,0 +1,5 @@
+C:\Users\c2072289\Documents\HelloWorldy\target\debug\deps\HelloWorldy.exe: src\main.rs
+
+C:\Users\c2072289\Documents\HelloWorldy\target\debug\deps\HelloWorldy.d: src\main.rs
+
+src\main.rs:
